4b9eab72e5ff503ef1ee8aaeab9a9d29c84e6ad9
codetraffics <eboat086@gmail.com>
2023-10-16 02:25:04 -0100

    Added the % of getting 8 heads and 2 tails

--- coin_flipping.js
+++ coin_flipping.js
@@ -1,14 +1,37 @@
-let coins = 10;
-let heads = 0;
-let tails = 0;
+/* Flip 10 coins multiple times and determine the %  
+   of getting exactly 8 heads and 2 tails.
+*/
 
-for(let i = 0; i < coins; i++) {
-    if(Math.random() >= 0.5) {
-        heads += 1;
+const flip_coin = () => {
+    let coins = 10;
+    let heads = 0;
+    let tails = 0;
+
+    for(let i = 0; i < coins; i++) {
+        if(Math.random() < 0.5) {
+            heads += 1;
+        }
+        else {
+            tails += 1;
+        }
+    }
+
+    if(heads == 8) {
+        return true;
     }
     else {
-        tails += 1;
+        return false;
     }
 }
 
-console.log(heads, "heads", tails, "tails");
+let repeats = 10000;
+let counter = 0;
+
+for(let i = 0; i < repeats; i++) {
+    let desired_outcome = flip_coin()
+    if(desired_outcome) {
+        counter += 1;
+    }
+}
+
+console.log("Getting 8 heads and 2 tails is", (counter / repeats) * 100, "% of the time")
